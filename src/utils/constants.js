fa081f60efd6128209bd7371a714167fc62b500f
amaanraaj <amandwivedi00@gmail.com>
2023-06-29 23:56:46 +0530

    home page allign

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const API_KEY = "AIzaSyDPxuHcMm-B5ZCYAGUr4udFvzKAfTuHp60"
-export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+API_KEY;
+export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=52&regionCode=US&key="+API_KEY;
 export const YOUTUBE_SEARCH_API = "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const YOUTUBE_SEARCH_SUGGESTIONS_API = "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key="+ API_KEY + "&q=";
 
